c571c705576db06b06a0c8a3ed86baa646589dbb
queenanshi21-beep <queenanshi21@gmail.com>
2026-09-03 12:28:59 +0300

    Add utility functions and main program

--- src/main.py
+++ src/main.py
@@ -4,5 +4,10 @@ from utils import square, is_even, celsius_to_fahrenheit
 number = float(input("Enter a number: "))
 
 print("Square:", square(number))
-print("Even:" if is_even(number) else "Odd:", is_even(number))
+
+if is_even(number):
+    print("Even: True")
+else:
+    print("Even: False")
+
 print("Fahrenheit:", celsius_to_fahrenheit(number))
